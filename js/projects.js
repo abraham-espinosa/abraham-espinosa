a5a0b2a575913e8146d299d55a4cd2853d42cd48
abraham-1 <esp19005@byui.edu>
2024-01-16 14:41:00 -0600

    Update projects.js skills.js and add new svg files

--- js/projects.js
+++ js/projects.js
@@ -18,6 +18,9 @@ fetch('js/projects.json')
         let siteImg = document.createElement('img');
         let githubImg = document.createElement('img');
 
+        let aYoutube = document.createElement('a');
+        let youtubeImg = document.createElement('img');
+
         let div = document.createElement('div');
 
         title.textContent = projects[i].title;
@@ -36,11 +39,20 @@ fetch('js/projects.json')
         githubImg.alt = "site-logo"
         aGithub.appendChild(githubImg);
 
+        aYoutube.href = projects[i].youtubeUrl;
+        aYoutube.target = "_blank";
+        youtubeImg.src = projects[i].youtubeImg;
+        youtubeImg.alt = "youtube-logo"
+        aYoutube.appendChild(youtubeImg);
+
         div.appendChild(title);
         div.appendChild(description);
         div.appendChild(technologies);
-        div.appendChild(aSite);
+        if (projects[i].siteUrl != "") {
+          div.appendChild(aSite);
+        }
         div.appendChild(aGithub);
+        div.appendChild(aYoutube);
         
 
         card.appendChild(div);
